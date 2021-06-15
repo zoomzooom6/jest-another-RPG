48b44924d7abca398d20507c74ee08579a066069
zoomzoom6 <tonyhuang107@live.ca>
2021-06-15 13:15:21 -0400

    create Player object constructor and getStats and getInvetory methods

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -14,3 +14,22 @@ test('creates a player object', () => {
         expect.arrayContaining([expect.any(Object)])
     );
 });
+
+test("gets player's stats as an object", () => {
+    const player = new Player('Banana');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Floof');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
+})
