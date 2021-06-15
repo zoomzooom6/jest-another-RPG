3f88e481552d8a7a91a69c4149413cff3160aa78
zoomzoom6 <tonyhuang107@live.ca>
2021-06-15 15:43:33 -0400

    create additional methods for Player object

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -32,4 +32,33 @@ test('gets inventory from player or returns false', () => {
     player.inventory = [];
 
     expect(player.getInventory()).toEqual(false);
-})
+});
+
+test("gets player's health value", () => {
+    const player = new Player('Monkey');
+
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+});
+
+test('check if player is alive or not', () => {
+    const player = new Player('Banana');
+
+    expect(player.isAlive()).toBeTruthy();
+
+    player.health=0;
+
+    expect(player.isAlive()).toBeFalsy();
+});
+
+test("subtracts from player's health", () => {
+    const player = new Player('Monkey');
+    const oldHealth = player.health;
+
+    player.reduceHealth(5);
+
+    expect(player.health).toBe(oldHealth - 5);
+
+    player.reduceHealth(99999);
+
+    expect(player.health).toBe(0);
+});
